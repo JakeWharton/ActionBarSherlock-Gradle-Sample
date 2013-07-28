7ccde6907dbaf54f6a5b876e6a02fbe8a3c1340f
Jake Wharton <jw@squareup.com>
2013-07-28 04:01:53 -0700

    Initial commit.

---
+++ src/main/java/com/android/tests/basic/Main.java
@@ -0,0 +1,11 @@
+package com.example.actionbarsherlock.gradle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import android.os.Bundle;
+
+public class Main extends SherlockActivity {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
